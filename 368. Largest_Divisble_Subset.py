73ae3c8f4a9f5bdbf50a0b5aa748230ba86e528d
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-04-06 21:42:40 +0530

    Create 368. Largest_Divisble_Subsets.py

---
+++ 368. Largest_Divisble_Subset.py
@@ -0,0 +1,47 @@
+Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
+
+answer[i] % answer[j] == 0, or
+answer[j] % answer[i] == 0
+If there are multiple solutions, return any of them.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,2]
+Explanation: [1,3] is also accepted.
+Example 2:
+
+Input: nums = [1,2,4,8]
+Output: [1,2,4,8]
+ 
+
+##########################   Solution   #############################
+
+
+class Solution:
+  def largestDivisibleSubset(self, nums: list[int]) -> list[int]:
+    n = len(nums)
+    ans = []
+    count = [1] * n
+    prevIndex = [-1] * n
+    maxCount = 0
+    index = -1
+
+    nums.sort()
+
+    for i, num in enumerate(nums):
+      for j in reversed(range(i)):
+        if num % nums[j] == 0 and count[i] < count[j] + 1:
+          count[i] = count[j] + 1
+          prevIndex[i] = j
+      if count[i] > maxCount:
+        maxCount = count[i]
+        index = i
+
+    while index != -1:
+      ans.append(nums[index])
+      index = prevIndex[index]
+
+    return ans
