430de05fdf0465433c157ae2b07d07f673660982
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-05-14 15:27:54 +0530

    Create Graph_Day_2 .py
    Breath-First-Search

---
+++ Graph_Day_2 .py
@@ -0,0 +1,149 @@
+                                                       TOPIC ---- Breadth First search
+
+Leetcode:100
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Example 1:
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+ 
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+
+#################################### Solution ---BFS   ############################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        queue = deque()
+        queue.append((p,q))
+
+        while queue:
+            node1, node2 = queue.popleft()
+
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+
+        return True
+
+##################################################################################################
+Leetcode: 226
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+##########################  Solution----BFS  ########################################3
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+
+            # swap the left and right children
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        return root
+
+
+############################################################################################
+Leetcode:101
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+Example 2:
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+###################### Solution -- BFS ######################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        queue = deque()
+        queue.append((root.left, root.right))
+
+        while queue:
+            t1, t2 = queue.popleft()
+
+            if not t1 and not t2:
+                continue
+            if not t1 or not t2:
+                return False
+            if t1.val != t2.val:
+                return False
+
+            queue.append((t1.left, t2.right))
+            queue.append((t1.right, t2.left))
+
+        return True
+        
+ 
+
+        
