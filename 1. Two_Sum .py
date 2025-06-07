208a0cabde6a0b22fbd04a832521a29b2593cf33
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-06-07 23:32:34 +0530

    Update 1. Two_Sum .py

--- 1. Two_Sum .py
+++ 1. Two_Sum .py
@@ -4,8 +4,6 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order.
 
- 
-
 Example 1:
 
 Input: nums = [2,7,11,15], target = 9
@@ -42,8 +40,3 @@ class Solution:
             if x in hash:
                 return [hash[x], i]
             hash[num] = i
-
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
