46e02df370dab694f0e8f9084a7ee22279021d05
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-06-17 22:19:05 +0530

    Update 73. Set_Matrix_Zero.py

--- 73. Set_Matrix_Zero.py
+++ 73. Set_Matrix_Zero.py
@@ -12,7 +12,6 @@ Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 
 ############ SOLUTION ##############
-
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         n = len(matrix)
@@ -32,4 +31,3 @@ class Solution:
                     matrix[i][j] = 0
 
         return matrix
-        
