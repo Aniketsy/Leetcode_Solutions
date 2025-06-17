50febdb422b3a42b46a781ce055ebf91fb3be405
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-06-17 22:17:19 +0530

    Update 1123. Lowest_Common_Ancestor_of_Deepest_Leaves.py

--- 1123. Lowest_Common_Ancestor_of_Deepest_Leaves.py
+++ 1123. Lowest_Common_Ancestor_of_Deepest_Leaves.py
@@ -1,7 +1,6 @@
 Given the root of a binary tree, return the lowest common ancestor of its deepest leaves.
 
-Recall that:
-
+ Recall that:
 The node of a binary tree is a leaf if and only if it has no children
 The depth of the root of the tree is 0. if the depth of a node is d, the depth of each of its children is d + 1.
 The lowest common ancestor of a set S of nodes, is the node A with the largest depth such that every node in S is in the subtree with root A.
@@ -43,6 +42,7 @@ class Solution:
                 return (left_depth + 1, node)
         
         return dfs(root)[1]
+     
         
 
 
