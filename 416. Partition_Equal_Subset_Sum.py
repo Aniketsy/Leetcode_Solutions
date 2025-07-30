c02f7e550059b21596863046faff63619c85c99a
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-07-31 05:22:38 +0530

    Update 416. Partition_Equal_Subset_Sum.py

--- 416. Partition_Equal_Subset_Sum.py
+++ 416. Partition_Equal_Subset_Sum.py
@@ -1,7 +1,5 @@
 Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
 
- 
-
 Example 1:
 
 Input: nums = [1,5,11,5]
@@ -13,13 +11,10 @@ Input: nums = [1,2,3,5]
 Output: false
 Explanation: The array cannot be partitioned into equal sum subsets.
  
-
 Constraints:
 
 1 <= nums.length <= 200
 1 <= nums[i] <= 100
-
-
 
 ####################### Solution  ##################
 class Solution:
